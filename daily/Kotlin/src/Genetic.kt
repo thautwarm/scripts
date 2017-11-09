0953992571dbf3d868eb54ac0806dc7b1dca6fd2
thautwarm <twshere@outlook.com>
2017-11-09 20:34:59 +0800

    ok

--- daily/Kotlin/src/Genetic.kt
+++ daily/Kotlin/src/Genetic.kt
@@ -10,33 +10,53 @@ object Genetic {
                   chromosomeSize: Int,
                   mutateRate: Double = 0.2,
                   crossoverRate: Double = 0.1) {
-        val ChromosomeSize = chromosomeSize
-        val GroupSize = groupSize
-        val MutateRate = mutateRate
-        val CrossoverRate = crossoverRate
-        val Fit = fit;
+        private val ChromosomeSize = chromosomeSize
+        private val GroupSize = groupSize
+        private val MutateRate = mutateRate
+        private val CrossoverRate = crossoverRate
+        private val Fit = fit;
         private val GetCrossoverHead = { prob: Double -> (prob * rnd.nextDouble()).toInt() }
         private fun GenChromosome(size: Int) = IntRange(0, size).map { rnd.nextBoolean() }
-        var Popularity = IntRange(0, GroupSize).map{ GenChromosome(it) }
-        
-        fun Mutate(c: Chromosome): Chromosome = c.map { it ->
+        var Popularity = IntRange(0, GroupSize).map{ GenChromosome(chromosomeSize) }
+        var BestInd = GenChromosome(chromosomeSize)
+        private var swap = BestInd
+
+        private fun Mutate(c: Chromosome): Chromosome = c.map { it ->
             if (rnd.nextDouble() > MutateRate) {
                 !it
             }
             it
         }
 
-        fun Crossover(c1: Chromosome, c2: Chromosome): Pair<Chromosome, Chromosome> {
+        private fun Crossover(c1: Chromosome): Chromosome {
+            if (rnd.nextDouble()>CrossoverRate) return c1
             val head = GetCrossoverHead(rnd.nextDouble())
-            return c1.zip(c2).mapIndexed { idx, gene ->
-                if (idx <= head)
-                    Pair<Boolean, Boolean>(gene.second, gene.first)
-                gene
-            }.unzip()
+            return c1.zip(swap).mapIndexed { idx, (first, second) ->
+                if (idx <= head){
+                    swap = c1
+                    second
+                }
+                else first
+            }
         }
 
-        fun evolution(order: Int) {
-            Popularity
+        private fun evolution(order: Int) {
+            println("""$order-th cycle.""")
+
+            Popularity = Popularity
+                    .map { Mutate(it) }
+                    .map { Crossover(it) }
+                    .sortedBy(Fit)
+                    .mapIndexed{
+                        idx, chromosome->
+                            if(idx<GroupSize/2) chromosome
+                            else GenChromosome(ChromosomeSize)
+                    }
+            BestInd = Popularity.get(0)
+        }
+        fun NaturalSelection(iter:Int=100) = {
+            IntRange(0, iter).forEach{evolution(it)}
+            BestInd
         }
 
     }
