4c3b6b61d309dddbff9e1e7f7286d1e792adebff
thautwarm <twshere@outlook.com>
2017-11-09 18:59:12 +0800

    naive GA for java

--- daily/Java/src/Console.java
+++ daily/Java/src/Console.java
@@ -6,23 +6,22 @@ import java.util.stream.IntStream;
 
 public class Console {
 
-    public static void Perceptron(String[] args){
+    private static void Perceptron(String[] args) {
         int dataSize = 100, featureLength = 2;
 
-        if (args.length == 2){
+        if (args.length == 2) {
             dataSize = Integer.parseInt(args[0]);
             featureLength = Integer.parseInt(args[1]);
-        }
-        else if(args.length == 1){
+        } else if (args.length == 1) {
             dataSize = Integer.parseInt(args[0]);
         }
 
         Perceptron perceptron = new Perceptron(2);
         List<Tuple<Vector<Double>, Double>> datas = DataGen.DataSets(dataSize, featureLength, i -> -1 + i.get(0) + i.get(1));
 
-        IntStream.range(0, 3).forEach(cycle->{
-            datas.forEach(data->{
-                    perceptron.Renew(data.fst, data.snd);
+        IntStream.range(0, 3).forEach(cycle -> {
+            datas.forEach(data -> {
+                perceptron.Renew(data.fst, data.snd);
             });
 
         });
@@ -30,34 +29,34 @@ public class Console {
         Vector<Double> prediction = new Vector<>(dataSize);
 
 
-        for (Tuple<Vector<Double>, Double> data: datas){
+        for (Tuple<Vector<Double>, Double> data : datas) {
             prediction.add(perceptron.Forward(data.fst));
         }
 
 
-        try{
+        try {
             FileWriter dataIO = new FileWriter("./data.txt");
             FileWriter predIO = new FileWriter("./predict.txt");
-            datas.forEach(data->{
+            datas.forEach(data -> {
 
-                data.fst.forEach(i->{
+                data.fst.forEach(i -> {
                     try {
-                        dataIO.write(i.toString()+",");
+                        dataIO.write(i.toString() + ",");
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
 
                 });
                 try {
-                    dataIO.write(data.snd.toString()+"\n\r");
+                    dataIO.write(data.snd.toString() + "\n\r");
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             });
 
-            prediction.forEach(i->{
+            prediction.forEach(i -> {
                 try {
-                    predIO.write(i.toString()+"\n\r");
+                    predIO.write(i.toString() + "\n\r");
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -67,15 +66,12 @@ public class Console {
             dataIO.close();
             predIO.close();
 
-        }
-        catch (FileNotFoundException e){
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         switch (args.length) {
             case 1:
@@ -94,14 +90,14 @@ public class Console {
         Genetic();
     }
 
-    public static void Genetic(){
+    private static void Genetic() {
 
-        Genetic genetic = new Genetic(data-> (data.get(0)==true?1.0:0)
-                                            +(data.get(1)==false?1.0:0)
-                                            +(data.get(2)==true?1.0:0),
+        Genetic genetic = new Genetic(data -> (data.get(0) ? 1.0 : 0)
+                + (!data.get(1) ? 1.0 : 0)
+                + (data.get(2) ? 1.0 : 0),
                 100, 3);
         genetic.NaturalSelection(100);
-        genetic.BestIndividual.forEach(i->System.out.print(i+","));
+        genetic.BestIndividual.forEach(i -> System.out.print(i + ","));
         System.out.println();
 
     }
